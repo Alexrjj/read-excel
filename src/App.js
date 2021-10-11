d11f96ac3e31d89d8040891d8a914efbeb002d8e
Alexrjj <alexsoares2517@gmail.com>
2021-10-10 23:37:57 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as XLSX from "xlsx";
+import { useState } from "react";
 
 function App() {
+  const [Items, setItems] = useState([]);
+
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+
+      <table className="table container">
+        <thead>
+          <tr>
+            <th scope="col">Produto</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Total</th>
+            <th scope="col">Custo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Items.map((d, index) => (
+            <tr key={index}>
+              {console.log(d)}
+              <th>{d.Produto}</th>
+              <td>
+                {d.Valor !== undefined
+                  ? parseFloat(d.Valor).toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })
+                  : null}
+              </td>
+              <td>{d.Quantidade}</td>
+              <td>
+                {d.Total !== undefined
+                  ? parseFloat(d.Total).toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })
+                  : null}
+              </td>
+              <td>
+                {d.Custo !== undefined
+                  ? parseFloat(d.Custo).toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })
+                  : null}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
